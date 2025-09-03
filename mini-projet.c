d62d6a959748ecfcdd70723ce15b5f04bd9d2c0f
Houssam Bensiyed <houssam.harts12@gmail.com>
2025-09-03 20:38:51 +0100

    implementer les prototypes

--- mini-projet.c
+++ mini-projet.c
@@ -23,3 +23,46 @@ void supprimerLivre();
 void afficherTotalStock();
 void viderBuffer();
 
+int main() {
+    int choix;
+    
+    printf("\n========================================\n");
+    printf("   SYSTEME DE GESTION DE LIBRAIRIE     \n");
+    printf("             Created By: Houssam Bensiyed\n");
+    printf("========================================\n");
+    
+    do {
+        afficherMenu();
+        printf("Votre choix : ");
+        scanf("%d", &choix);
+        viderBuffer();
+        
+        switch(choix) {
+            case 1:
+                ajouterLivre();
+                break;
+            case 2:
+                afficherTousLesLivres();
+                break;
+            case 3:
+                rechercherLivre();
+                break;
+            case 4:
+                mettreAJourQuantite();
+                break;
+            case 5:
+                supprimerLivre();
+                break;
+            case 6:
+                afficherTotalStock();
+                break;
+            case 0:
+                printf("\nMerci d'avoir utilise notre systeme. Au revoir!\n");
+                break;
+            default:
+                printf("\nChoix invalide! Veuillez reessayer.\n");
+        }
+    } while(choix != 0);
+    
+    return 0;
+}
